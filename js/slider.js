01ad12114af832197ff0fb582d56225ef08496c9
Oksana Bashynska <oksanavvisotska@gmail.com>
2021-02-18 16:22:52 +0200

    slider 2

--- js/slider.js
+++
@@ -1,6 +0,0 @@
- $('.cd-testimonials-wrapper').flexslider({ selector: ".cd-testimonials > li",
-  animation: "slide", controlNav: false, slideshow: false, smoothHeight: true,
-  start: function(){ $('.cd-testimonials').children('li').css({ 'opacity': 1,
-  'position': 'relative' }); } });
-  $('.cd-testimonials-all-wrapper').children('ul').masonry({ itemSelector:
-  '.cd-testimonials-item' });
